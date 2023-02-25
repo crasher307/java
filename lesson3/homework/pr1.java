d2ea7cdc8a4b7bd0318e9b8ee939ec6f022ab1ab
crasher307 <crasher307@gmail.com>
2023-02-25 10:13:58 +0500

    lesson 3 homework

---
+++ lesson3/homework/pr1.java
@@ -0,0 +1,49 @@
+// Реализовать алгоритм сортировки слиянием
+
+package lesson3.homework;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class pr1 {
+    private static final Random rand = new Random();
+
+    public static void main(String[] args) {
+        int[] num = Create(10, 100, 20);
+        String data = "create:\t" + Arrays.toString(num) + "\nresult:\t" + Arrays.toString(MergeSort(num)) + "\n";
+        System.out.println(data);
+    }
+
+    private static int[] Create(int min, int max, int count) {
+        var num = new int[count];
+        for (int i = 0; i < count; i++) num[i] = min + rand.nextInt(max - min + 1);
+        return num;
+    }
+
+    private static int[] MergeSort(int[] array) {
+        return MergeSort(array, new int[array.length], 0, array.length, false);
+    }
+
+    private static int[] MergeSort(int[] array, boolean log) {
+        return MergeSort(array, new int[array.length], 0, array.length, log);
+    }
+
+    private static int[] MergeSort(int[] buf1, int[] buf2, int start, int end, boolean log) {
+        if (start >= end - 1) return buf1;
+        // Разбиение
+        int mid = start + (end - start) / 2;
+        int[] b1 = MergeSort(buf1, buf2, start, mid, log);
+        int[] b2 = MergeSort(buf1, buf2, mid, end, log);
+        // Сортировка
+        int[] result = b1 == buf1 ? buf2 : buf1;
+        int i1 = start, i2 = mid, i3 = start;
+        while (true) {
+            if (i1 < mid && i2 < end) result[i3++] = b1[i1] < b2[i2] ? b1[i1++] : b2[i2++];
+            else if (i1 < mid) result[i3++] = b1[i1++];
+            else if (i2 < end) result[i3++] = b2[i2++];
+            else break;
+        }
+        if (log) System.out.println("Res: " + Arrays.toString(result));
+        return result;
+    }
+}
